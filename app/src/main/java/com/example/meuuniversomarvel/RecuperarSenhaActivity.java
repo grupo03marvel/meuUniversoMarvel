8d60c455aabf5282e8ba62e041a90cd9ad477d2c
Filipe <fil.desouza@gmail.com>
2019-09-24 20:13:08 -0300

    Adições Filipe

---
+++ app/src/main/java/com/example/meuuniversomarvel/RecuperarSenhaActivity.java
@@ -0,0 +1,14 @@
+package com.example.meuuniversomarvel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecuperarSenhaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperar_senha);
+    }
+}
