7cbde04daee3505e88488a38b2e193bfe1a2ce8a
Lucassap <lucassap01@gmail.com>
2019-09-30 18:33:46 -0300

    Nova development. Débito Técnico Corrigido

---
+++ app/src/main/java/com/example/meuuniversomarvel/splashActivity.java
@@ -0,0 +1,33 @@
+package com.example.meuuniversomarvel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class splashActivity extends AppCompatActivity {
+
+    private Timer timer = new Timer();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                jump();
+            }
+        },5000);
+    }
+
+    private void jump(){
+        timer.cancel();
+        startActivity(new Intent(splashActivity.this, LoginActivity.class));
+        finish();
+    }
+}
