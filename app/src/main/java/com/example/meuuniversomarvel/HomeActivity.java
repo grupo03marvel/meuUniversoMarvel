1569af5fe6099a228ff4b254f2b357c2460b3249
ArielOliveiraS <gildaariel@outlook.com>
2019-10-01 16:28:49 -0300

    Tela Home com buttons

--- app/src/main/java/com/example/meuuniversomarvel/HomeActivity.java
+++ app/src/main/java/com/example/meuuniversomarvel/HomeActivity.java
@@ -3,12 +3,57 @@ package com.example.meuuniversomarvel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button btnQuadrinhos;
+    private Button btnPersonagens;
+    private Button btnAutores;
+    private Button btnSeries;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        initView();
+
+        btnSeries.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnQuadrinhos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnPersonagens.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnAutores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
+
+    public void initView(){
+        btnAutores.findViewById(R.id.buttonAutores);
+        btnPersonagens.findViewById(R.id.buttonPersonagens);
+        btnQuadrinhos.findViewById(R.id.buttonQuadrinhos);
+        btnSeries.findViewById(R.id.buttonSeries);
+
     }
 }
